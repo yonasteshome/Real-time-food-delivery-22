f01978b7ee916b90f27bae3678ce4f38441f4253
Yonas zekarias <130641535+YonasZekarias@users.noreply.github.com>
2025-08-20 22:35:21 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,8 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 // Middlewares
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
