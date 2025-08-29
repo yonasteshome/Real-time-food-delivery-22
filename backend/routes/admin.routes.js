8f08ebf4e59072e61d53fd1bc5da8f743ed5290e
yohanzack <yohanzack9@gmail.com>
2025-08-29 06:33:47 +0300

    edited restaurant otp settings

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const {
   getAllUsers,
+  pendingRestaurant,
+  verifyRestaurant,
   rejectRestaurant,
   getUserByRoles,
   getAllRestaurants,
@@ -16,12 +18,9 @@ const router = express.Router();
 
 router.get("/users", protect, restrictTo("admin"), getAllUsers);
 router.post("/users/roles", protect, restrictTo("admin"), getUserByRoles);
-router.patch(
-  "/restaurants/:id/reject",
-  protect,
-  restrictTo("admin"),
-  rejectRestaurant
-);
+router.get("/restaurants/pending", protect, restrictTo("admin"), pendingRestaurant);
+router.patch("/restaurants/:id/verify", protect, restrictTo("admin"), verifyRestaurant);
+router.patch("/restaurants/:id/reject", protect, restrictTo("admin"), rejectRestaurant);
 router.get("/restaurants", protect, restrictTo("admin"), getAllRestaurants);
 router.get("/stats", protect, restrictTo("admin"), getPlatformStats);
 router.patch("/users/:id/suspend", protect, restrictTo("admin"), suspendUser);
