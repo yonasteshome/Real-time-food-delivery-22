ba9713f3edb955496867db21b78405489dbc3b3d
Yonas Teshome <yoyoni826@gmail.com>
2025-09-18 13:03:00 +0300

    done

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 for frontend
-=======
-# Real-time-food-delivery-
->>>>>>> 54b57a6912fc2346b47ccfbfa4558c1689356400
--- frontend/src/api/restaurant/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://real-time-food-delivery.onrender.com/api/delivery/restaurants",
-  withCredentials: true,
-});
-
-export default api;
