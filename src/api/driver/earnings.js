6a00a4fbade1884090e3a6910454956d64e26db3
Yonas Teshome <140391850+yonasteshome@users.noreply.github.com>
2025-09-19 22:15:24 +0300

    Update earnings.js

--- src/api/driver/earnings.js
+++ src/api/driver/earnings.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const fetchDriverEarningsAPI = async (driverId) => {
   try {
     const res = await axios.get(
-      `https://real-time-food-delivery.onrender.com/api/delivery/drivers/${driverId}/earnings`,
+      `https://delivery-backend-jtub.onrender.com/api/delivery/drivers/${driverId}/earnings`,
       { withCredentials: true }
     );
     return res.data.data;
