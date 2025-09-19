35a43a5fd064d95bf4f0753ed724cc4dedc4d35e
Yonas Teshome <140391850+yonasteshome@users.noreply.github.com>
2025-09-19 22:02:13 +0300

    Update orderApi.js

--- src/api/customer/orderApi.js
+++ src/api/customer/orderApi.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || "https://real-time-food-delivery.onrender.com/api/delivery";
+const API_URL = process.env.REACT_APP_API_URL || "https://delivery-backend-jtub.onrender.com/api/delivery";
 
 export const createOrder = async (orderData) => {
   const response = await fetch(`${API_URL}/orders`, {
